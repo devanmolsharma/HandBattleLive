da08eb7bdafdeb6a721433121e48b2c66f81b150
keep-up-sharma <keepupsharma@gmail.com>
2023-12-27 22:33:42 -0600

    Code broken down into functions

--- Frontend/RPS.js
+++ Frontend/RPS.js
@@ -1,16 +1,17 @@
 const model = handPoseDetection.SupportedModels.MediaPipeHands;
 const detectorConfig = {
     runtime: 'mediapipe',
-    solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/hands'
+    solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/hands',
 };
 
-// Basic settings for the video to get from Webcam
 const constraints = {
     video: true,
     audio: false,
 };
 
-
+let detector;
+let canvas;
+let ctx;
 
 function checkGesturePosition(hand) {
     const numFingersUP = hand.numFingersUp;
@@ -24,44 +25,45 @@ function checkGesturePosition(hand) {
         return "Invalid hand position";
     }
 }
+
+function initializeVideo() {
+    const video = document.getElementById('video');
+
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(function (stream) {
+                video.srcObject = stream;
+                setInterval(() => {
+                    drawOnCanvas(ctx, video);
+                    sendFrame(canvas.toDataURL("image/jpeg"));
+                }, 100);
+
+                setInterval(processHandEstimation, 300);
+            })
+            .catch(function (error) {
+                console.log("Error accessing webcam:", error);
+            });
+    }
+}
+
+async function processHandEstimation() {
+    canvas.width = video.videoWidth * 0.3;
+    canvas.height = video.videoHeight * 0.3;
+    const hands = await detector.estimateHands(video);
+    const parsedHands = HandParser.parse(hands);
+
+    if (hands.length === 1) {
+        document.getElementById("position").innerText = checkGesturePosition(parsedHands[0]);
+    }
+}
+
+function drawOnCanvas(ctx, video) {
+    ctx.drawImage(video, 0, 0, video.videoWidth * 0.3, video.videoHeight * 0.3);
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     detector = await handPoseDetection.createDetector(model, detectorConfig);
     canvas = document.createElement('canvas');
     ctx = canvas.getContext('2d');
-    var video = document.getElementById('video');
-    
-    
-    
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia(constraints)
-        .then(function (stream) {
-            
-            video.srcObject = stream;
-            setInterval(() => {
-                drawOnCanvas(ctx, video)
-                sendFrame(canvas.toDataURL("image/jpeg"));
-            }, 100);
-            setInterval(async () => {
-                canvas.width = video.videoWidth*0.3;
-                canvas.height = video.videoHeight*0.3;
-                    const hands = await detector.estimateHands(video);
-                    const parsedHands = HandParser.parse(hands);
-                    if (hands.length == 1) {
-                        document.getElementById("position").innerText = checkGesturePosition(parsedHands[0]);
-                    }
-                }, 300);
-            })
-            .catch(function (err0r) {
-                console.log("Something went wrong!");
-            });
-    }
-
-})
-
-
-function drawOnCanvas(ctx, video) {
-    ctx.drawImage(video, 0, 0,video.videoWidth*0.3, video.videoHeight*0.3);
-}
-
-
-
+    initializeVideo();
+});
